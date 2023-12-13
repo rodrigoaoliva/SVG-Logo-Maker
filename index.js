c8c4ade9d21a95ec90921682fda87916c7df447c
rodrigoaoliva <rodrigoaoliva1@gmail.com>
2023-12-13 17:54:20 -0300

    Create an instance of the selected shape class based on user input

---
+++ index.js
@@ -0,0 +1,24 @@
+const { getUserInput } = require('./lib/userInterface');
+const { writeToFile } = require('./lib/fileWriter');
+const { Triangle, Circle, Square } = require('./lib/shapes');
+
+const generateLogo = async () => {
+    const userInput = await getUserInput();
+  
+    let shape;
+    switch (userInput.shape) {
+      case 'Circle':
+        shape = new Circle();
+        break;
+      case 'Triangle':
+        shape = new Triangle();
+        break;
+      case 'Square':
+        shape = new Square();
+        break;
+      default:
+        console.error('Invalid shape');
+        return;
+    }
+
+}
